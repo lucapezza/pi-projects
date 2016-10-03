234595f5945f7176ece93512544fc22e9814d56f
Luca Pezzarossa <l.pezzarossa@gmail.com>
2016-10-03 23:55:44 +0000

    Fixing the git ignoring list.

--- accelerometer/adxl345_v1.sh
+++ accelerometer/adxl345_v1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gcc adxl345_v1.c -o adxl345_v1 -lwiringPi
+gcc adxl345_v1.c -o adxl345_v1.o -lwiringPi
 
-./adxl345_v1
+./adxl345_v1.o
 
 gnuplot -e "plot './data.dat' using 1:2 title 'X' with line, './data.dat' using 1:3 title 'Y' with line, './data.dat' using 1:4 title 'Z' with line; pause(-1)"
